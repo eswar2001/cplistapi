dd8b992f8ab9f54278cd39d6ac0716bf99b4f4b6
eswar2001 <teswar2001@gmail.com>
2021-03-07 17:04:32 +0530

    codechef bug fixed

--- src/parsers/codechef.js
+++ src/parsers/codechef.js
@@ -5,7 +5,6 @@ function parseContestDetails($, contestRow) {
   const details = $(contestRow).find('td');
   const startTime = new Date(details.eq(2).attr("data-starttime")).getTime() / 1000;
   const endTime = new Date(details.eq(3).attr("data-endtime")).getTime() / 1000;
-
   return {
     name: details.eq(1).text(),
     url: `http://www.codechef.com${details.eq(1).find('a').attr('href')}`,
@@ -20,16 +19,15 @@ const codechef = () => axios.get('http://www.codechef.com/contests', { timeout: 
     const $ = cheerio.load(response.data);
     const statusdiv = $('table .dataTable');
     const headings = $('h2');
-    const contestTables = { 'Upcoming Coding Contests': [], 'Present Coding Contests': [] };
+    const contestTables = { 'Upcoming Coding Contests': [], 'Present Coding Contests': [], 'Past Coding Contests': [] };
 
     for (let i = 0; i < headings.length; i++) {
-      if (headings.eq(i).text() !== 'Past Coding Contests') {
-        contestTables[headings.eq(i).text()] = statusdiv.eq(i).find('tr').slice(1);
+      if (headings.eq(i).text() !== 'Past Coding Contests' && headings.eq(i).text() !== 'Site Navigation') {
+        contestTables[headings.eq(i).text()] = statusdiv.eq(i - 1).find('tr').slice(1);
       }
     }
     let contests = contestTables['Present Coding Contests'].map((i, elem) => parseContestDetails($, elem)).get();
     contests = contests.concat(contestTables['Upcoming Coding Contests'].map((i, elem) => parseContestDetails($, elem)).get());
-
     return contests;
   })
   .catch((error) => {
